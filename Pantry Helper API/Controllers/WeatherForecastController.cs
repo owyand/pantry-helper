d52cf5edceeb85d61db298579e08cb6b672e7038
oliver wyand <owyand22@gmail.com>
2026-06-28 20:54:33 -0400

    setting up project again after making updates

--- Pantry Helper API/Controllers/WeatherForecastController.cs
+++ Pantry Helper API/Controllers/WeatherForecastController.cs
@@ -17,7 +17,7 @@ namespace Pantry_Helper_API.Controllers
         {
             _logger = logger;
         }
-
+        /*
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
@@ -28,6 +28,6 @@ namespace Pantry_Helper_API.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
-        }
+        }*/
     }
 }
